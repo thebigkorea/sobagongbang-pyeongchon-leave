73f26d2b78ecdde3a8764c48f0af93cf6aaa41c2
thebigkorea <thebig@koreafnb.com>
2026-05-03 18:58:47 +0900

    final sobagongbang version

--- service-worker.js
+++ service-worker.js
@@ -1,23 +1,36 @@
-const CACHE_NAME = "sobagongbang-pyeongchon-leave-v3";
+const CACHE_NAME="sobagongbang-pyeongchon-leave-v5";
 
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll([
-        "./index.html",
-        "./admin.html",
-        "./style.css",
-        "./script.js",
-        "./manifest.json"
-      ]);
-    })
-  );
+self.addEventListener("install",event=>{
+
+event.waitUntil(
+
+caches.open(CACHE_NAME).then(cache=>{
+
+return cache.addAll([
+"./index.html",
+"./admin.html",
+"./style.css",
+"./script.js",
+"./manifest.json"
+]);
+
+})
+
+);
+
 });
 
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
+
+self.addEventListener("fetch",event=>{
+
+event.respondWith(
+
+caches.match(event.request).then(response=>{
+
+return response||fetch(event.request);
+
+})
+
+);
+
 });
